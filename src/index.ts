a0bed45d2c08b2422e1a8b1adba8e5f498ff4c8e
gjulianni <gabrieljulianni24@gmail.com>
2025-09-18 21:43:02 -0300

    conexão com o banco mysql

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import { PrismaClient } from '@prisma/client';
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+// ===================== MIDDLEWARES =====================
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cookieParser());
+
+async function startServer() {
+
+    const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+    try {
+        
+        await prisma.$queryRaw`SELECT 1`;
+        console.log("✅ Conectado ao MySQL com sucesso");
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Servidor rodando em ${process.env.BACKEND_URL}:${PORT}`);
+        });
+    } catch (error) {
+        console.error(`❌ Erro ao iniciar servidor: `, error);
+        process.exit(1);
+    }
+}
+
+startServer();
