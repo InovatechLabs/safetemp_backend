426195f8df5e64f152d58d478eeae78106e022f9
gjulianni <gabrieljulianni24@gmail.com>
2025-11-15 17:17:06 -0300

    feat: funçao para ativar alerta

--- src/routes/user/alerts/alertsRoutes.ts
+++ src/routes/user/alerts/alertsRoutes.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { deleteAlert, disableAlert, list, listUserAlerts, registerAlert, saveUserToken } from "../../../controllers/user/alerts/alertsController";
+import {
+    deleteAlert,
+    disableAlert,
+    enableAlert,
+    list,
+    listUserAlerts,
+    registerAlert,
+    saveUserToken
+} from "../../../controllers/user/alerts/alertsController";
 import { authenticate } from "../../../middlewares/auth";
 
 const alertsRouter = Router();
@@ -10,6 +18,7 @@ alertsRouter.get('/list', authenticate, listUserAlerts);
 
 alertsRouter.delete('/delete/:id', authenticate, deleteAlert);
 alertsRouter.put('/disable/:id', authenticate, disableAlert);
+alertsRouter.put('/enable/:id', authenticate, enableAlert);
 alertsRouter.get('/listall', list);
 
 export default alertsRouter;
