479281429f26ec3605c83db4350f631c5047078d
gjulianni <gabrieljulianni24@gmail.com>
2025-11-21 02:39:37 -0300

    puppeteer fixes

--- src/services/reportService.ts
+++ src/services/reportService.ts
@@ -2,19 +2,35 @@ import puppeteer from "puppeteer";
 import { reportTemplate } from "../utils/templates/reportsTemplate";
 
 export async function generateReportPDF(report: any) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
 
-    const html = reportTemplate(report);
-
-    await page.setContent(html, { waitUntil: "networkidle0" });
-
-    const pdf = await page.pdf({
-        format: "A4",
-        printBackground: true
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage", 
+            "--single-process", 
+            "--no-zygote",
+        ],
     });
 
-    await browser.close();
+    try {
+        const page = await browser.newPage();
+        const html = reportTemplate(report);
 
-    return pdf;
+        await page.setContent(html, { waitUntil: "networkidle0" });
+
+        const pdf = await page.pdf({
+            format: "A4",
+            printBackground: true
+        });
+
+        return pdf;
+
+    } catch (error) {
+        console.error("Erro no Puppeteer:", error);
+        throw error;
+    } finally {
+        if (browser) await browser.close(); 
+    }
 }
