0852181209fc4955044aee470df5e1fb856f3e59
gjulianni <gabrieljulianni24@gmail.com>
2026-01-23 21:10:39 -0300

    fix funçao finalizar experimento, permite apenas quem criou finalizar

--- src/controllers/user/experiments/experimentsController.ts
+++ src/controllers/user/experiments/experimentsController.ts
@@ -49,18 +49,32 @@ export const ExperimentoController = {
   },
 
 
-  async finalizar(req: Request, res: Response) {
+  async finalizar(req: AuthenticatedRequest, res: Response) {
     const { id } = req.params;
 
-    try {
-      const finalizado = await prisma.experimento.update({
-        where: { id: Number(id) },
-        data: { ativo: false, data_fim: new Date() }
-      });
-      return res.json(finalizado);
-    } catch (error) {
-      return res.status(500).json({ error: "Erro ao finalizar experimento." });
+    if(!req.user) return res.status(400).json({ message: 'Usuário não autenticado.' });
+    const userId = req.user?.id; 
+
+    const numberedId = Number(id);
+      if (isNaN(numberedId)) {
+      return res.status(400).json({ message: 'ID de experimento inválido.' });
     }
+
+    const experiment = await prisma.experimento.findFirst({
+      where: { 
+      id: numberedId,
+      userId: userId,
+      ativo: true
+      }
+    })
+    if (!experiment) return res.status(404).json({ message: 'Experimento não encontrado' });
+
+  
+    await prisma.experimento.update({
+        where: { id: experiment.id },
+        data: { ativo: false }
+      });
+      return res.json({ message: 'Experimento finalizado com sucesso.' });
   },
 
   async buscarAtivoPorDevice(req: Request, res: Response) {
